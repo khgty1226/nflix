3ff3c49e348e5e466328243b0177994c550adb21
khgty1226 <khgty1226@naver.com>
2025-12-09 16:57:19 +0900

    8.15 Final Project part One (layoutId 활용. overlay 위로 box 선택 확대)

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import {
 } from "framer-motion";
 import { useState} from "react";
 
-const Wrapper = styled(motion.div)`
+const Wrapper = styled.div`
     width: 100vw;
     height: 100vh;
     display: flex;
@@ -14,15 +14,21 @@ const Wrapper = styled(motion.div)`
     background: linear-gradient(135deg, rgb(238, 0, 153), rgb(221, 0, 238));
 `;
 
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  width: 50vw;
+  gap: 10px;
+  div:first-child,
+  div:last-child {
+    grid-column: span 2;
+  }
+`;
+
 const Box = styled(motion.div)`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    color: black;
-    width: 400px;
-    height: 400px;
     background-color: rgba(255, 255, 255, 1);
     border-radius: 40px;
+    height: 200px;
     box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
@@ -34,21 +40,39 @@ const Circle = styled(motion.div)`
     box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
+const Overlay = styled(motion.div)`
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const overlay = {
+    hidden: { backgroundColor: "rgba(0, 0, 0, 0)" },
+    visible: { backgroundColor: "rgba(0, 0, 0, 0.5)" },
+    exit: { backgroundColor: "rgba(0, 0, 0, 0)" },
+};
+
 function App() {
     const [clicked, setClicked] = useState(false);
     const click = () => setClicked((prev) => !prev);
     return (
         <Wrapper onClick={click}>
-            <Box>
-                {!clicked ? (
-                    <Circle layoutId="circle" style={{ borderRadius: 50 }} />
-                ) : null}
-            </Box>
-            <Box>
+            <Grid>
+                <Box layoutId="box"></Box>
+                <Box></Box>
+                <Box></Box>
+                <Box></Box>
+            </Grid>
+            <AnimatePresence>
                 {clicked ? (
-                    <Circle layoutId="circle" style={{ borderRadius: 0, scale: 2 }} />
+                    <Overlay variants={overlay} initial="hidden" animate="visible" exit="exit">
+                        <Box layoutId="box" style={{width:400, height:400}}></Box>
+                    </Overlay>
                 ) : null}
-            </Box>
+            </AnimatePresence>
         </Wrapper>
     );
 }
