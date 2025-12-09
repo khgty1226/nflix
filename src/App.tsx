e6d480c2be882f407bd57e2584440e0e0beb5751
khgty1226 <khgty1226@naver.com>
2025-12-09 11:53:03 +0900

    8.6 Gestures part Two

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import {motion, Variants} from "framer-motion";
+import {useRef} from "react";
 
 const Wrapper = styled.div`
     height: 100vh;
@@ -7,6 +8,16 @@ const Wrapper = styled.div`
     display: flex;
     justify-content: center;
     align-items: center;
+`;
+
+const BiggerBox = styled.div`
+  width: 600px;
+  height: 600px;
+  background-color: rgba(255, 255, 255, 0.4);
+  border-radius: 40px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
 const Box = styled(motion.div)`
@@ -18,22 +29,25 @@ const Box = styled(motion.div)`
 `;
 
 const boxVariants: Variants = {
-    hover: { scale: 1.5, rotateZ: 90 },
-    click: { scale: 1, borderRadius: "100px" },
-    drag: { backgroundColor: "rgb(46, 204, 113)", transition: { duration: 3 } },
+    hover: { rotateZ: 90 },
+    click: { borderRadius: "100px" }
 };
 
 function App() {
+    const biggerBoxRef = useRef<HTMLDivElement>(null);
     return (
         <Wrapper>
-            <Box
-                drag
-                variants={boxVariants}
-                 whileHover="hover"
-                 whileDrag="drag"
-                 whileTap="click"
-            >
-            </Box>
+            <BiggerBox ref={biggerBoxRef}>
+                <Box
+                    drag
+                    // dragSnapToOrigin
+                    dragElastic={0.5}
+                    dragConstraints={biggerBoxRef}
+                    variants={boxVariants}
+                    whileHover="hover"
+                    whileTap="click"
+                />
+            </BiggerBox>
         </Wrapper>
     );
 }
