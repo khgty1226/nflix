ab223e0f09895dad1a85dc158a0ad6a8fe946054
khgty1226 <khgty1226@naver.com>
2025-12-15 18:14:35 +0900

    불필요한 import 제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import Home from "./components/Home"
-import MovieRouter from "./apps/nflix/MovieRouter";
 import Tv from "./components/Tv";
 import Search from "./components/Search";
 import Header from "./components/Header";
